2cff188021098fc61396842cdccd550a89977928
Ken Vilar <kenvilar@gmail.com>
2018-09-26 22:06:59 +0800

    Add and delete todo implementation

--- app/controllers.js
+++ app/controllers.js
@@ -3,5 +3,33 @@
 var app = angular.module('kenTodoApp.controller', []);
 
 app.controller('todoController', ['$scope', function($scope) {
-	$scope.newTask = '';
+	$scope.newTodo = '';
+
+	$scope.todoList = [
+		{
+			desc: 'Make a coffee',
+			done: false,
+		},
+		{
+			desc: 'Read a book',
+			done: false,
+		},
+		{
+			desc: 'Study AngularJS',
+			done: false,
+		},
+	];
+
+	$scope.addTodo = () => {
+		$scope.todoList.push({
+			desc: $scope.newTodo,
+			done: false,
+		});
+
+		$scope.newTodo = '';
+	};
+
+	$scope.deleteTodo = (todo) => {
+		$scope.todoList.splice(todo, 1)
+	};
 }]);
